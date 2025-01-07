13398199702f466a65871a5311273e81cb8de6e1
jey fason <jeyfason58@gmail.com>
2025-01-07 02:07:58 +0200

    jey fason

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,12 +2,13 @@ import { useNavigate } from "react-router-dom"
 import "./Notfound.css"
 
 function NotFound() {
+    const navigate = useNavigate()
   return (
     <div className="notFound">
         <h1>404: Page Not Found</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
 
-        <button>Go Back</button>
+        <button onClick={()=>navigate("/")}>Go Back</button>
     </div>
   )
 }
